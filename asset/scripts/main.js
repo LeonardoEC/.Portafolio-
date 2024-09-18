de813800236e5cfd07e4c452762e1be021e8231c
LeonardoEc <LeonardoDanielAV@proton.me>
2024-09-18 08:58:06 +0200

    Testeo

--- asset/scripts/main.js
+++ asset/scripts/main.js
@@ -4,7 +4,7 @@
 
 // Importaciones componentes
 import InicioComponents from "../../components/inicio.js";
-//import SobreMiComponents from "../../components/sobreMi.js";
+import SobreMiComponents from "../../components/sobreMi.js";
 //import ProyectoContainer from "../../components/proyecto.js";
 //import HabilidadComponent from "../../components/habilidad.js";
 //import ContactComponent from "../../components/Contacto.js";
@@ -15,7 +15,7 @@ import InicioComponents from "../../components/inicio.js";
 
 // componentes individuales
 const inicioContent = InicioComponents.renderInicio();
-//const sobreMiConntent = SobreMiComponents.renderSobreMi();
+const sobreMiConntent = SobreMiComponents.renderSobreMi();
 //const proyectoContent = ProyectoContainer.renderProyecto();
 //const habilidadContent = HabilidadComponent.renderHabilidad();
 //const contactoContent = ContactComponent.renderContactos();
@@ -26,7 +26,7 @@ const inicioContent = InicioComponents.renderInicio();
 
 // renderizado individual
 document.getElementById("presentation").innerHTML = inicioContent;
-//document.getElementById("sobre-mi").innerHTML = sobreMiConntent;
+document.getElementById("sobre-mi").innerHTML = sobreMiConntent;
 //document.getElementById("proyectos").innerHTML = proyectoContent;
 //document.getElementById("habilidades").innerHTML = habilidadContent;
 //document.getElementById("contacto").innerHTML = contactoContent;
--- components/sobreMi.js
+++ components/sobreMi.js
@@ -3,16 +3,71 @@ import settingsLanguages from "../../asset/data/settingsPages/settingsLanguages.
 
 class SobreMiComponents {
 
+  static aboutMeDataPage =
+    {
+      title: "Sobre mi",
+      sectionOne: {
+        skills:
+        {
+          title: "Mi perfil",
+          text: "Como técnico superior en desarrollo web y aplicaciones móviles, he adquirido habilidades sólidas en programación y diseño de interfaces. Sin embargo, mi pasión por los videojuegos me ha llevado a especializarme en este ámbito. Me encanta la programación y disfruto del proceso de transformar ideas en experiencias interactivas. Soy un aprendiz autodidacta y estoy siempre dispuesto a asumir nuevos desafíos, ya sea aprendiendo nuevas tecnologías o resolviendo problemas complejos."
+        },
+        softSkills: {
+          title: "Habilidades blandas",
+          text: "Soy una persona responsable y comprometida con mis tareas. Me caracterizo por mi honestidad y mi ética de trabajo. No tengo miedo de enfrentar nuevos desafíos y siempre estoy dispuesto a aprender cosas nuevas. Además, soy un gran trabajador en equipo y me adapto fácilmente a diferentes situaciones. Me gusta ayudar a mis compañeros y creo que la colaboración es clave para alcanzar el éxito."
+        },
+      },
+      sectionTwo: {
+        achievements: {
+          medals: {
+            MinHub:
+            {
+              frontend:
+              {
+                title: "FrondEnd",
+                img: "https://images.credly.com/size/680x680/images/db619416-19a0-4c5b-b4f7-7e44d097f392/image.png",
+                url: "https://www.credly.com/badges/cb76c166-2774-45e9-97bf-ea9b66453d5a/public_url"
+              },
+              mern:
+              {
+                title: "MERN",
+                img: "https://images.credly.com/size/680x680/images/be67019e-9d37-432b-bc29-18cdcb85917b/image.png",
+                url: "https://www.credly.com/badges/f619fdef-d01c-4820-bc0f-af7f70fb2840/public_url"
+              }
+            },
+            GitHub:
+            {
+              formacion:
+              {
+                title: "GitHub",
+                img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/200px-Google_Drive_icon_%282020%29.svg.png",
+                url: "https://drive.google.com/drive/folders/1C_Ujm3LAD61hIDgvLXkAKcprumt_yEhe"
+              }
+            },
+            General:
+            {
+              formacion:
+              {
+                title: "Certificados Academicos",
+                img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/200px-Google_Drive_icon_%282020%29.svg.png",
+                url: "https://drive.google.com/drive/folders/1C_Ujm3LAD61hIDgvLXkAKcprumt_yEhe"
+              }
+            },
+          }
+        }
+      }
+    }
 
-    static aboutMeData = settingsLanguages.sp.pagesData.AboutMe;
+
+  static aboutMeData = aboutMeDataPage;
 
 
-    static sectionOne = settingsLanguages.sp.pagesData.AboutMe.sectionOne;
-    static sectionTwo = settingsLanguages.sp.pagesData.AboutMe.sectionTwo;
+  static sectionOne = aboutMeDataPage.sectionOne;
+  static sectionTwo = aboutMeDataPage.sectionTwo;
 
 
-    static allSkillsMap = Object.entries(this.sectionOne).map(([key, value]) => {
-        return `
+  static allSkillsMap = Object.entries(this.sectionOne).map(([key, value]) => {
+    return `
           <div class="gradiant-border">
             <div class="card-sobre-mi">
               <h3 class="card-title-sobre-mi">${value.title}</h3>
@@ -20,10 +75,10 @@ class SobreMiComponents {
             </div>
           </div>
         `;
-    }).join('');
+  }).join('');
 
-    static achievements = Object.entries(this.sectionTwo.achievements.medals).map(([platform, medals]) => {
-        return `
+  static achievements = Object.entries(this.sectionTwo.achievements.medals).map(([platform, medals]) => {
+    return `
           <div class="card-logros">
             <h2>${platform}</h2>
             <div class="card-data-logros">
@@ -36,13 +91,13 @@ class SobreMiComponents {
               </div>
           </div>
         `;
-    }).join('');
+  }).join('');
 
 
 
-    static renderSobreMi() {
+  static renderSobreMi() {
 
-        return `
+    return `
                     <section class="title-sobre-mi">
                         <h2 class="gradient-text">${this.aboutMeData.title}</h2>
                     </section>
@@ -63,7 +118,7 @@ class SobreMiComponents {
                     </div>
                 `;
 
-    }
+  }
 }
 
 export default SobreMiComponents;
