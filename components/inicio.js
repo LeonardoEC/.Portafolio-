4eed22141278b7ef756e8d61d9d30caa1cdabd98
LeonardoEc <LeonardoDanielAV@proton.me>
2024-09-18 08:31:20 +0200

    mas intentos

--- components/inicio.js
+++ components/inicio.js
@@ -1,25 +1,22 @@
-import settingsLanguages from "../asset/data/settingsPages/settingsLanguages.js";
 
 class InicioComponents {
 
-    static Home= {
+    static homeDataPage= {
         img:"../foto/Final.png",
         title: "Leonardo Daniel Vivas",
         text: "Saludos y bienvenido a mi portafolio, Soy un estudiante de Desarrollo web y mi stack es M.E.R.N. Estoy en búsqueda de un puesto de trabajo como programador para mejorar mis conocimientos, habilidades y obtener más experiencia en el sector IT"
     }
 
-    static homeMeData = settingsLanguages.sp.pagesData.Home;
-
     static renderInicio() {
         return `
             <div class="row-presentacion">
                 <div class="col-2-top-presentacion">
-                    <img class="img-presentacion" src="${this.Home.img}" alt="img">
+                    <img class="img-presentacion" src="${this.homeDataPage.img}" alt="img">
                 </div>
                 <div class="col-1-top-presentacion">
-                    <h1 class="title-presentacion">${this.Home.title}</h1>
+                    <h1 class="title-presentacion">${this.homeDataPage.title}</h1>
                     <p class="text-presentacion">
-                        ${this.Home.text}
+                        ${this.homeDataPage.text}
                     </p>
                 </div>
             </div>
